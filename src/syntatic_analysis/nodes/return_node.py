9880d4f54833448afb1356005e27a7fe33b5abf9
Carol Aguiar <acrla@academico.ufpb.br>
2025-09-28 02:39:07 -0300

    [FIX] Replace 'ret' with register move for _start entrypoint compatibility

--- src/syntatic_analysis/nodes/return_node.py
+++ src/syntatic_analysis/nodes/return_node.py
@@ -10,5 +10,5 @@ class ReturnNode(BaseNode):
 
     def generate_code(self):
         code = self.expression.generate_code()
-        code += "\nret\n"
+        code += "\nmov %rax, %rdi\n"
         return code
